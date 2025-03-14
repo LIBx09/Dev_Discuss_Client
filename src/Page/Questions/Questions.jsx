84af2b76586558d25e917ee21b2a5ae16b5a3aa7
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2025-03-14 15:39:45 +0600

    landing page added

---
+++ src/Page/Questions/Questions.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Questions = () => {
+  return (
+    <div>
+      <h2 className="text-xl font-bold">This is the Questions Page</h2>
+    </div>
+  );
+};
+
+export default Questions;
---
+++ src/Page/Saves/Saves.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Saves = () => {
+    return (
+        <div>
+            saves
+        </div>
+    );
+};
+
+export default Saves;
