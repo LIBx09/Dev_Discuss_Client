a4df9b613bcefcefc32461258bce131addaa7576
Libx09 <ibrahimfuad729@gmail.com>
2025-03-21 03:55:43 +0600

    dark light toggle implement

--- src/MainLayout/Sidebars/LeftSidebar.jsx
+++ src/MainLayout/Sidebars/LeftSidebar.jsx
@@ -50,21 +50,3 @@ const LeftSidebar = () => {
 };
 
 export default LeftSidebar;
-
-//     return (
-//         <div className="h-full py-6 px-4 border-r border-r-gray-500">
-//             <ul className="space-y-3">
-//                 <li className="flex items-center gap-2 hover:bg-gray-100 "><span className="font-bold"><AiFillHome /></span>Home</li>
-//                 <li className="flex items-center gap-2 hover:bg-gray-100"><span><FaCircleQuestion /></span>Questions</li>
-//                 <li className="flex items-center gap-2 hover:bg-gray-100 pb-6"><span><IoMdPricetags /></span>Tags</li>
-//                 <li className="flex items-center gap-2 hover:bg-gray-100"><span><FaBookmark /></span>Saves</li>
-//                 <li className="flex items-center gap-2 hover:bg-gray-100"><span><TbLogs /></span>Blogs</li>
-//                 <li className="flex items-center gap-2 hover:bg-gray-100 pb-6"><span><MdEventNote /></span>Event</li>
-//                 <li className="flex items-center gap-2 hover:bg-gray-100"><span><FaUsers /></span>Users</li>
-//             </ul>
-
-//         </div>
-//     );
-// };
-
-// export default LeftSidebar;
--- src/components/DarkLight/DarkLightToggle.jsx
+++ src/components/DarkLight/DarkLightToggle.jsx
@@ -1,30 +1,25 @@
 import { useEffect, useState } from "react";
 
 const DarkLightToggle = () => {
-  const [darkMode, setDarkMode] = useState(
-    localStorage.getItem("theme") === "dark"
-  );
+  const storedTheme = localStorage.getItem("theme") || "light";
+  const [theme, setTheme] = useState(storedTheme);
 
   useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    }
-  }, [darkMode]);
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
-    <div className="form-control">
-      <label className="label cursor-pointer">
-        <input
-          type="checkbox"
-          className="toggle"
-          checked={darkMode}
-          onChange={() => setDarkMode(!darkMode)}
-        />
-      </label>
-    </div>
+    <input
+      type="checkbox"
+      checked={theme === "dark"}
+      className="toggle"
+      onChange={toggleTheme}
+    />
   );
 };
 
