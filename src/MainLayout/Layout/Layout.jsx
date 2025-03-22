456c2ea59849c4202c9b78e26e2580b2fb59380f
Libx09 <ibrahimfuad729@gmail.com>
2025-03-22 22:18:17 +0600

    dark light func bug fix

--- src/MainLayout/Layout/Layout.jsx
+++ src/MainLayout/Layout/Layout.jsx
@@ -7,28 +7,19 @@ const Layout = () => {
   return (
     <div className="w-[98%] md:w-11/12 mx-auto dark:bg-slate-950 dark:text-white ">
       <Navbar />
-<<<<<<< HEAD
       <div className="relative min-h-screen flex flex-col md:flex-row  dark:bg-gray-900 dark:text-white py-6 gap-6">
-=======
-      <div className="relative min-h-screen flex flex-col md:flex-row bg-white  py-6 gap-6">
-        
->>>>>>> 65e4790369b350cf9766cfc9bf24e1d4ecab34c5
         {/* ✅ Left Sidebar */}
-        <div className="w-full  md:w-1/8">
+        <div className="w-full md:w-1/8">
           <LeftSidebar />
         </div>
 
         {/* ✅ Main Content Section */}
-<<<<<<< HEAD
         <div className="flex-1  dark:bg-gray-950 shadow-lg rounded-xl p-6">
-=======
-        <div className="flex-1 bg-white  shadow-lg rounded-xl p-6">
->>>>>>> 65e4790369b350cf9766cfc9bf24e1d4ecab34c5
           <Outlet />
         </div>
 
         {/* ✅ Right Sidebar */}
-        <div className="w-full md:w-1/5 dark:bg-gray-950 dark:text-white hidden md:block">
+        <div className="w-full md:w-1/5 dark:bg-gray-950  hidden md:block">
           <RightSidebar />
         </div>
       </div>
--- src/MainLayout/Sidebars/LeftSidebar.jsx
+++ src/MainLayout/Sidebars/LeftSidebar.jsx
@@ -7,15 +7,29 @@ import { TbLogs } from "react-icons/tb";
 
 const LeftSidebar = () => {
   return (
-
     <div>
       <div className="dropdown">
         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            {" "}
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />{" "}
+          </svg>
         </div>
         <ul
           tabIndex={0}
-          className="menu menu-sm dropdown-content bg-gray-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+          className="menu menu-sm dropdown-content bg-gray-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+        >
           <li className="flex items-center gap-2 hover:bg-gray-100 p-2 rounded-md">
             <Link to="/" className="flex items-center gap-2 w-full">
               <AiFillHome className="text-lg" /> Home
@@ -93,12 +107,7 @@ const LeftSidebar = () => {
         </ul>
       </div>
     </div>
-
   );
 };
 
 export default LeftSidebar;
-<<<<<<< HEAD
-=======
-
->>>>>>> 65e4790369b350cf9766cfc9bf24e1d4ecab34c5
