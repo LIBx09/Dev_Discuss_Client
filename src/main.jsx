404a7415c1ee62f393a8b7f79f5864c283b0a445
mohibbullah272 <bullahmohi50@gmail.com>
2025-04-08 16:23:35 +0600

    add loading page implement react query and polish some area

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,17 @@ import router from "./Routes";
 import { RouterProvider } from "react-router-dom";
 import ContextProvider from "./Context/ContextProvider";
 import { Toaster } from "react-hot-toast";
-
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ContextProvider>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+    </QueryClientProvider>
     </ContextProvider>
     <Toaster></Toaster>
   </StrictMode>
